a8a32ae14b7edc8a917ff6e7bf96144a9858fb6f
mcthoren <thoren.mcdole@gmail.com>
2022-05-04 18:24:50 +0000

    first pass at a generalized script to start collection temperature data across all pis

---
+++ box_temp.go
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+OS="$(uname -s)"
+HOST_N="$(hostname -s)"
+tmp_f="$(mktemp)" || exit 1
+
+MYHOME="/home/ghz"
+[ "${OS}" = "Linux" ] && {
+	[ -d "/import/home/ghz" ] && MYHOME="/import/home/ghz"
+}
+
+dst_d="${MYHOME}/box_temp/"
+dst_f="${dst_d}/${HOST_N}_temp.48h"
+
+arch_d="${dst_d}/${HOST_N}/$(date -u +"%Y")"
+arch_f="${arch_d}/${HOST_N}_temp.$(date -u +"%F")"
+
+for tst_d in "${arch_d}" "${dst_d}"; do
+	[ -d "${tst_d}" ] || mkdir -p "${tst_d}"
+	[ -d "${tst_d}" ] || {
+		echo "can't create ${tst_d}"
+		exit 1
+	}
+done
+
+[ "${OS}" = "OpenBSD" ] && temp="$(sysctl -n hw.sensors.bcmtmon0.temp0)"
+
+[ "${OS}" = "Linux" ] && {
+	temp=$(awk '{temp = ($1) / 1000; printf "%0.2f degC\n", temp}'  \
+	/sys/class/thermal/thermal_zone0/temp)
+}
+
+dat_s="$(date -u +"%FT%TZ")\t${temp}\n"
+printf "${dat_s}" >> "${dst_f}"
+printf "${dat_s}" >> "${arch_f}"
+
+# 48h * 60min/h = 2880
+tail -2880 "${dst_f}" > "${tmp_f}"
+mv "${tmp_f}" "${dst_f}"
